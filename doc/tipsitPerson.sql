9e6ee7f9afe9f45cd4b3ecc61d450c626f511a7a
Mathyptr <mathyptr@gmail.com>
2021-01-24 17:09:59 +0100

    Inserimento dump sql

---
+++ doc/tipsitPerson.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS tipsit;
+CREATE TABLE IF NOT EXISTS tipsit.person(id int(11) NOT NULL AUTO_INCREMENT, firstName varchar(50) NOT NULL, lastName varchar(50) NOT NULL,address text DEFAULT NULL,  passport varchar(50) DEFAULT NULL,  PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+INSERT INTO tipsit.person (id, firstName, lastName, address, passport) VALUES (1, 'Mathilde', 'Patrissi', 'Via....', NULL),(2, 'Mat', 'Pat', 'Via----', NULL);
+
+
+CREATE USER 'mathy'@'localhost' IDENTIFIED BY 'mathy';
+GRANT select ON tipsit.person TO 'mathy'@'localhost';
